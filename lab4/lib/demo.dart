7444723d0e0894535f7e5cce68e4194207c2cce6
Hemanth-mamilla <hemanthmamilla09@gmail.com>
2025-03-13 13:46:09 +0530

    Add files via upload

---
+++ lab4/lib/demo.dart
@@ -0,0 +1,21 @@
+class switchdemo(
+void sdemo(String name){
+         switch(name)
+
+   case 'other':
+          continue first;   
+   case 'red':
+         print("it is red");
+   case 'black':
+   case 'Black':
+         print("it is black");
+   
+   first 
+	case 'green':
+               print("it is green");
+    default:
+           print("colour not mentioned"):
+     }
+ }
+}
+      
